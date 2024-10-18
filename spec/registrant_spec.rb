d86c86fe7a1ee2997cacd8408e9e6f3db93c66af
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-19 00:13:21 +0200

    Added and passed all tests for Registrant class, including edge cases

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -33,4 +33,38 @@ RSpec.describe Registrant do
     # Test if the permit status is updated
     expect(registrant_2.permit?).to eq(true)
   end
+
+  # Edge Case 1: Registrants of any age can earn a permit
+  it 'allows registrants of any age to earn a permit' do
+    registrant_young = Registrant.new('Baby Bruce', 5)
+    registrant_old = Registrant.new('Grandpa Joe', 85)
+
+    # Initially, neither should have a permit
+    expect(registrant_young.permit?).to eq(false)
+    expect(registrant_old.permit?).to eq(false)
+
+    # Both earn a permit
+    registrant_young.earn_permit
+    registrant_old.earn_permit
+
+    # Both should now have a permit
+    expect(registrant_young.permit?).to eq(true)
+    expect(registrant_old.permit?).to eq(true)
+  end
+
+  # Edge Case 2: Permit defaults to false
+  it 'defaults permit to false if not provided' do
+    registrant_3 = Registrant.new('Charlie', 30)
+
+    # Permit should be false by default
+    expect(registrant_3.permit?).to eq(false)
+  end
+
+  # Edge Case 3: Initialize with a permit if provided
+  it 'can initialize with a permit if provided' do
+    registrant_1 = Registrant.new('Bruce', 18, true)
+
+    # Permit should be true because it was provided as true
+    expect(registrant_1.permit?).to eq(true)
+  end
 end
