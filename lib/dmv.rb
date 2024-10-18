65afa1efa392a23cc7d05575e8754a88d256a4f1
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-18 22:19:11 +0200

    Added detailed comments to Dmv class and fixed facilities access

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,16 +1,23 @@
 class Dmv
+  # This creates a getter method for the facilities attribute, 
+  # allowing the list of facilities to be accessed from outside the class 
+  # but not modified directly. This ensures data encapsulation.
+  attr_reader :facilities
 
+  # The initialize method is the constructor for the Dmv class.
+  # It is called when a new instance of Dmv is created.
+  # It initializes an empty array for @facilities, which will later 
+  # store the list of facilities managed by the Dmv.
   def initialize
-    @facilities = []
+    @facilities = []  # Initializes an empty array to hold facilities.
   end
 
+  # This method allows a facility to be added to the list of facilities.
+  # The facility is passed as an argument and appended to the @facilities array.
+  # This method ensures that the Dmv object can dynamically manage multiple facilities.
+  #
+  # @param facility [Object] the facility object that will be added to the @facilities array.
   def add_facility(facility)
-    @facilities << facility
-  end
-
-  def facilities_offering_service(service)
-    @facilities.find do |facility|
-      facility.services.include?(service)
-    end
+    @facilities << facility  # Adds the provided facility to the @facilities array.
   end
 end
